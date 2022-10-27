58734effd0de3417cb9df784f9caab22a9838359
Dennis Diekmann <dennis.diekmann@uni-oldenburg.de>
2022-10-27 10:32:18 +0200

    Setup docker to be usable without sudo

--- Installations/docker.sh
+++ Installations/docker.sh
@@ -5,7 +5,13 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
 
+#Setup docker for use without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
+
+#Install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
