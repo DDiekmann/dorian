6f3aef71a4707fb684136f5b5af78e31b3555ce1
Dennis Diekmann <dennis.diekmann@uni-oldenburg.de>
2023-02-18 14:01:53 +0100

    Change installation of docker compose

--- Installations/docker.sh
+++ Installations/docker.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/bash
+
 sudo apt update
 sudo apt install ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -13,5 +15,4 @@ newgrp docker
 docker run hello-world
 
 #Install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install docker-compose
