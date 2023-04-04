ab827d587da5d6b7b691afefebeb6eaefee384f9
Dennis Diekmann <dennis.diekmann@uni-oldenburg.de>
2023-04-04 11:36:51 +0200

    Add full wtf install

--- Installations/wtf/update_wtf.sh
+++ Installations/wtf/update_wtf.sh
@@ -1,13 +1,5 @@
 #! /usr/bin/bash
 
-# install wtf
-curl -o Dockerfile.build https://raw.githubusercontent.com/wtfutil/wtf/master/Dockerfile.build
-docker build -f Dockerfile.build -t wtfutil --build-arg=version=master .
-docker create --name wtf_build wtfutil
-docker cp wtf_build:/usr/local/bin/wtfutil ~/.local/bin
-docker rm wtf_build
-rm Dockerfile.build
-
 # copy config
 mkdir -p ~/.config/wtf
 cp ~/Dev/workysetup/Installations/wtf/wtf_config.yml ~/.config/wtf/config.yml
