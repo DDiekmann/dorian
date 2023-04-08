7ac98af808c5bb4af1dcae43162ef814712ec6da
Dennis <dennis.diekmann@uni-oldenburg.de>
2023-04-08 09:01:54 +0200

    Fix: No questions when installing stuff

--- Installations/docker.sh
+++ Installations/docker.sh
@@ -1,12 +1,13 @@
 #! /usr/bin/bash
 
 sudo apt update
-sudo apt install ca-certificates curl gnupg lsb-release
+sudo apt install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
 
 #Setup docker for use without sudo
 sudo groupadd docker
