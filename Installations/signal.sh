925e4bbb44e0711133915b67d8f0f076444d4407
Dennis <dennis.diekmann@uni-oldenburg.de>
2023-11-13 15:28:52 +0100

    Remove deprecated signal installation

--- Installations/signal.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
-
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | tee -a /etc/apt/sources.list.d/signal-xenial.list
-
-apt udpate
-apt install signal-desktop
